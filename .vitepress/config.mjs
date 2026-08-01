7973934511a1d1fd97d1185e68763929ebbd3b46
mahuicong <mahuicong@gmail.com>
2026-08-01 21:20:24 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,60 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "My Awesome Project",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    //配置目录显示级为2至4
+    outline:[2,4],
+    logo:"/logo.svg",
+    nav: [
+      { text: 'Home', items:[
+        {text:"首页",link: '/'},
+        {text:"markdonw 示例",link: '/markdonw-examples'}
+      ]},
+      { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+
+    //配置底部
+    footer:{
+      copyright:"Copyright@ 2026 Gavin Ma",
+    },
+    
+    //设置搜索框的样式
+    search: {
+      provider: "local",
+      options: {
+        translations: {
+          button: {
+            buttonText: "搜索文档",
+            buttonArialLabel: "搜索文档"
+          },
+          modal: {
+            noResultsText: "无法找到相关结果",
+            resetButtonTitle: "清除查询条件",
+            footer: {
+              selectText: "选择",
+              navigateText: "切换"
+            }
+          }
+        }
+      }
+    }
+  }
+})
---
+++ index.md
@@ -0,0 +1,28 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Gavin's Page"
+  text: "一个个人知识站点"
+  tagline: 不断学习 持续分享
+  image:
+    src: /background.svg
+    alt: 背景图
+  actions:
+    - theme: brand
+      text: Markdown Examples
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: Feature A
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature B
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: Feature C
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
+
