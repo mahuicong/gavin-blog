4d9917b8ccbca4c9ed00804609354ac8c339dda2
mahuicong <mahuicong@gmail.com>
2026-08-02 15:19:06 +0800

    在config.mjs新增base属性

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "My Awesome Project",
   description: "A VitePress Site",
+  base: '/gavin-blog/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     //配置目录显示级为2至4
